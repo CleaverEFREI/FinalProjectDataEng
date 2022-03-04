c12a6b987ade1fbe3c5840bc57408fe4ba7c11c2
Cleaver <louis.gailhac@efrei.net>
2022-03-04 23:37:49 +0100

    working test but not opti

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,5 +1,8 @@
 import pytest
 import requests
+import grequests
+import time
+import sys
 
 pytest_plugin = ["docker_compose"]
 
@@ -11,3 +14,22 @@ def test_homepage(homepage):
 
 def test_pred(homepage):
     assert 1 == 1
+
+@pytest.mark.anyio
+async def test_stress(homepage):
+
+    data = {
+      'inputdata': 'Testing'
+    }
+    
+    t_start = time.time()
+    
+    for i in range(10):
+        r = (grequests.post('http://localhost:5000/predict', data=data) for u in range(10))
+        assert len(grequests.map(r)) == 10    
+    t_end = time.time()
+
+    t = t_end-t_start
+
+    print("Info time :",t, file=sys.stderr)
+    assert t < 60
