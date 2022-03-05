6698b457e280c266aa3bf2049e1b8345e2cf3153
Cleaver <louis.gailhac@efrei.net>
2022-03-05 01:58:15 +0100

    working test but some werid bug to fix

--- tests/integration/test_app.py
+++ tests/integration/test_app.py
@@ -1,8 +1,8 @@
 import pytest
 import requests
 import grequests
-import time
-import sys
+import trio
+
 
 pytest_plugin = ["docker_compose"]
 
@@ -11,25 +11,16 @@ def test_homepage(homepage):
     assert requests.get(homepage).text.split("<h1>")[1].split(
         "</h1>")[0] == "Data Enginering Final Project"
 
-
-def test_pred(homepage):
-    assert 1 == 1
-
 @pytest.mark.anyio
-async def test_stress(homepage):
+async def test_stress():    
+    t_start =  trio.current_time()
+    t = trio.current_time()-t_start
+    count = 0
+    while t < 59 or count < 5000:        
+        r = (grequests.get('http://localhost:5000/') for u in range(20))
+        assert len(grequests.map(r)) == 20
+        count += 1
+        t = trio.current_time()-t_start
 
-    data = {
-      'inputdata': 'Testing'
-    }
-    
-    t_start = time.time()
-    
-    for i in range(10):
-        r = (grequests.post('http://localhost:5000/predict', data=data) for u in range(10))
-        assert len(grequests.map(r)) == 10    
-    t_end = time.time()
-
-    t = t_end-t_start
-
-    print("Info time :",t, file=sys.stderr)
-    assert t < 60
+    assert t < 61    
+    assert count >= 5000
